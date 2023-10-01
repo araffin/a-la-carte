cf00123ea1e008eac96697807729a13b96fa9938
Antonin Raffin <antonin.raffin@ensta.org>
2023-10-02 00:02:21 +0200

    Add slider and sort dishes

--- read.py
+++ read.py
@@ -1,5 +1,3 @@
-from pprint import pprint
-
 import yaml
 from jinja2 import Environment, FileSystemLoader
 
@@ -14,12 +12,15 @@ name_to_flags = {
     "france": "🇫🇷",
     "spicy": "🌶️",
 }
+n_dish_per_column = 7
+n_dish_per_page = 14
 for key in data.keys():
     with open(f"data/{key}.yaml") as f:
         raw_data = yaml.safe_load(f)
     # preprocess flags
     processed_data = []
-    for dish in raw_data:
+    raw_data = sorted(raw_data, key=lambda item: item["name"])
+    for idx, dish in enumerate(raw_data):
         processed_dish = dish.copy()
         flag_str = ""
         for tag in processed_dish.get("tags", []):
@@ -28,6 +29,10 @@ for key in data.keys():
 
         processed_dish["flags"] = flag_str
         processed_dish["options"] = dish.get("options", "TODO")
+        processed_dish["new_page"] = (idx % n_dish_per_page) == 0
+        processed_dish["close_page"] = idx > 0 and ((idx + 1) % n_dish_per_page) == 0 or idx == len(raw_data)
+        processed_dish["new_column"] = (idx % n_dish_per_column) == 0
+        processed_dish["close_column"] = idx > 0 and ((idx + 1) % n_dish_per_column) == 0 or idx == len(raw_data)
         processed_data.append(processed_dish)
 
     data[key] = processed_data
