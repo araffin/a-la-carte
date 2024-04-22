842f67716bc4c9e79cc5f2e63e53e13aa86f0d9a
Antonin Raffin <antonin.raffin@ensta.org>
2024-04-22 21:46:53 +0200

    Integrate backend

--- read.py
+++ read.py
@@ -1,25 +1,22 @@
 import re
+from pathlib import Path
 
-import yaml
 from jinja2 import Environment, FileSystemLoader
+from omg import Menu
 from PIL import Image
 
 NON_ALPHA_NUMERIC = re.compile(r"[^a-zA-Z0-9]")
 
-data: dict[str, list[dict]] = {
-    "main": [],
-    "dessert": [],
+current_dir = Path(__file__).parent
+
+menus: dict[str, Menu] = {
+    "main": Menu.load(f"{current_dir}/data/main.yaml", "main", "Main Course"),
+    "dessert": Menu.load(f"{current_dir}/data/dessert.yaml", "dessert", "Desserts"),
 }
-name_to_flags = {
-    "argentina": "🇦🇷",
-    "india": "🇮🇳",
-    "france": "🇫🇷",
-    "italy": "🇮🇹",
-    "afghanistan": "🇦🇫",
-    "spicy": "🌶️",
-}
+
+metadata: dict[str, list[dict]] = {}
+
 n_columns = 3
-n_pages = {}
 column_class = {
     1: "is-full",
     2: "is-half",
@@ -27,56 +24,35 @@ column_class = {
     4: "is-one-quarter",
 }
 total_recipes = 0
-for key in data.keys():
-    with open(f"data/{key}.yaml") as f:
-        raw_data = yaml.safe_load(f)
-
-    n_dish_per_column = len(raw_data) // n_columns + 1
+for key, menu in menus.items():
+    n_dish_per_column = len(menu.dishes) // n_columns + 1
     n_dish_per_page = n_columns * n_dish_per_column
-    total_recipes += len(raw_data)
+    total_recipes += len(menu.dishes)
 
-    n_pages[key] = 0
     # preprocess flags
-    processed_data = []
-    raw_data = sorted(raw_data, key=lambda item: item["name"])
-    for idx, dish in enumerate(raw_data):
-        processed_dish = dish.copy()
-        flag_str = ""
-        for tag in processed_dish.get("tags", []):
-            if tag in name_to_flags:
-                flag_str += " " + name_to_flags[tag]
+    processed_info = []
+    for idx, dish in enumerate(menu.dishes):
+        dish_info = {}
 
-        # convert name to an id by removing spaces and lowercasing
-        # and remove non-alphanumeric characters
-        dish_id = NON_ALPHA_NUMERIC.sub("", dish["name"].lower())
-
-        processed_dish["id"] = dish_id
-        processed_dish["tags"] = ",".join(processed_dish.get("tags", []))
-        processed_dish["ingredients"] = ",".join(processed_dish.get("ingredients", []))
-        processed_dish["flags"] = flag_str
-        processed_dish["link"] = dish.get("link")
-        processed_dish["image"] = dish.get("image")
-
-        if processed_dish["link"] and "http" not in processed_dish["link"]:
-            processed_dish["link"] = f"recipe.html?id={processed_dish['link']}"
+        if dish.link and "http" not in dish.link:
+            dish_info["link"] = f"recipe.html?id={dish.link}"
+        else:
+            dish_info["link"] = dish.link
 
         # Retrieve image dimensions by reading the image
-        if processed_dish["image"] and "https" not in processed_dish["image"]:
-            with Image.open(processed_dish["image"]) as img:
-                processed_dish["image_width"], processed_dish["image_height"] = img.size
+        if dish.image and "https" not in dish.image:
+            with Image.open(dish.image) as img:
+                dish_info["image_width"], dish_info["image_height"] = img.size
         else:
-            processed_dish["image_width"], processed_dish["image_height"] = 0, 0
+            dish_info["image_width"], dish_info["image_height"] = 0, 0
 
-        processed_dish["options"] = dish.get("options", "")
-        processed_dish["new_page"] = (idx % n_dish_per_page) == 0
-        processed_dish["close_page"] = idx > 0 and ((idx + 1) % n_dish_per_page) == 0 or idx == len(raw_data) - 1
-        processed_dish["new_column"] = (idx % n_dish_per_column) == 0
-        processed_dish["close_column"] = idx > 0 and ((idx + 1) % n_dish_per_column) == 0 or idx == len(raw_data) - 1
-        processed_data.append(processed_dish)
-        if processed_dish["new_page"]:
-            n_pages[key] += 1
+        dish_info["new_page"] = (idx % n_dish_per_page) == 0
+        dish_info["close_page"] = idx > 0 and ((idx + 1) % n_dish_per_page) == 0 or idx == len(menu.dishes) - 1
+        dish_info["new_column"] = (idx % n_dish_per_column) == 0
+        dish_info["close_column"] = idx > 0 and ((idx + 1) % n_dish_per_column) == 0 or idx == len(menu.dishes) - 1
+        processed_info.append(dish_info)
 
-    data[key] = processed_data
+    metadata[key] = processed_info
 
 
 # pprint(data)
@@ -86,10 +62,8 @@ environment = Environment(loader=FileSystemLoader("html/"))
 template = environment.get_template("template.html")
 
 content = template.render(
-    meals=[
-        {"name": "Main Course", "dishes": data["main"]},
-        {"name": "Desserts", "dishes": data["dessert"]},
-    ],
+    menus=menus,
+    metadata=metadata,
     column_class=column_class[n_columns],
     total_recipes=total_recipes,
 )
