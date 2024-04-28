024b4f87930bbc450d550f3899220c0a471fdcfa
Antonin Raffin <antonin.raffin@ensta.org>
2024-04-28 12:17:33 +0200

    Update script

--- new_dish.py
+++ new_dish.py
@@ -8,11 +8,13 @@ current_dir = Path(__file__).parent
 # List all the filenames in the images-tmp directory
 images = [item.name for item in (current_dir / "images-tmp").iterdir() if item.is_file()]
 
-menu = Menu.load(f"{current_dir}/data/dessert.yaml", "main", "Main Course")
+default_image = images[0] if len(images) == 1 else ""
+
+menu = Menu.load(f"{current_dir}/data/main.yaml", "main", "Main Course")
 
 try:
     name = Prompt.ask("Name?")
-    image = Prompt.ask("Image?", choices=images, default="")
+    image = Prompt.ask("Image?", choices=images, default=default_image)
     options = Prompt.ask("Options?", default="")
     ingredients = Prompt.ask("Ingredients?", default="").split(",")
     tags = Prompt.ask("Tags?", default="").split(",")
@@ -24,10 +26,16 @@ try:
     if tags == [""]:
         tags = []
 
+    # Remove extra spaces from ingredients and tags
+    ingredients = [ingredient.strip() for ingredient in ingredients]
+    tags = [tag.strip() for tag in tags]
+
     new_dish = Dish(name, ingredients=ingredients, tags=tags, options=options, link=link)
     if image:
         # Preprocess the image and save it to the images directory
         new_dish.update_image(current_dir / "images-tmp" / image, current_dir / "images")
+        # Move original image to processed folder
+        (current_dir / "images-tmp" / image).replace(current_dir / "images-tmp/processed" / image)
 
     menu.add(new_dish)
 
@@ -40,4 +48,4 @@ except KeyboardInterrupt:
     pass
 
 # Save the updated menu
-menu.save(f"{current_dir}/data/dessert.yaml")
+menu.save(f"{current_dir}/data/main.yaml")
