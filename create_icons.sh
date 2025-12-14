657debd628ef7888629cccbdae67d58c4d704cf0
Antonin RAFFIN <antonin.raffin@ensta.org>
2025-12-14 21:39:35 +0100

    Update icons

--- create_icons.sh
+++ create_icons.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 convert icons/pan_fight.png -resize 64x64 icons/android-chrome-64x64.png
+convert icons/pan_fight.png -resize 96x96 icons/favicon-96x96.png
 convert icons/pan_fight.png -resize 192x192 icons/android-chrome-192x192.png
 convert icons/pan_fight.png -resize 512x512 icons/android-chrome-512x512.png
 convert icons/pan_fight.png -resize 180x180 icons/apple-touch-icon-180x180.png
